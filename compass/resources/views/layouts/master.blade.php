6696283a608367928ccad43b94709c36973244d7
Aaron Parecki <aaron@parecki.com>
2015-09-22 12:56:42 -0700

    add libraries

--- compass/resources/views/layouts/master.blade.php
+++ compass/resources/views/layouts/master.blade.php
@@ -3,12 +3,11 @@
   <head>
     <meta charset="utf-8">
     <title>Compass</title>
-    <link rel="stylesheet" href="/assets/pure-0.6.0/pure-min.css">
-    <link rel="stylesheet" href="/assets/pure-0.6.0/grids-responsive-min.css">
-    <link rel="stylesheet" href="/assets/font-awesome-4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/assets/pure-min.css">
+    <link rel="stylesheet" href="/assets/font-awesome-4.4.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="/assets/styles.css">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script src="/assets/jquery.js"></script>
+    <script src="/assets/jquery-1.11.3.min.js"></script>
   </head>
   <body>
 
